f6d1ed86362b67c19a5ae8d9d2fd1188cb48483f
yetipakatha <educationsai98@gmail.com>
2024-08-24 19:12:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -113,13 +113,13 @@ OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'sampleupi@upi')
 
 # for stream
 IS_STREAM = is_enabled('IS_STREAM', True)
-BIN_CHANNEL = environ.get("BIN_CHANNEL", "-1002038146800")
+BIN_CHANNEL = environ.get("BIN_CHANNEL", "-1002019622279")
 if len(BIN_CHANNEL) == 0:
     print('Error - BIN_CHANNEL is missing, exiting now')
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "https://t.me/+5edW3asRiYc1ZjU1")
+URL = environ.get("URL", "https://t.me/+bD0BdNZCm60wY2Rl")
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
